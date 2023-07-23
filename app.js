06ea1fe9c33ec079d77cd5750f4e7ada38a05abc
Samuel Perez <sperez207@gmail.com>
2023-07-23 14:12:33 -0400

    Added BootStrap 5.0 styles and navbar/footer
    Added the alpha version of BootStrap and created a navbar and footer
    which is added to every view through partials. Also started adding
    some styles.

--- app.js
+++ app.js
@@ -23,16 +23,16 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('campgrounds/home');
 });
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds});
+    res.render('campgrounds/index', { campgrounds });
 });
 
 app.get('/campgrounds/new', (req, res) => {
@@ -47,22 +47,22 @@ app.post('/campgrounds', async (req, res) => {
 
 app.get('/campgrounds/:id/edit', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit', {campground})
+    res.render('campgrounds/edit', { campground })
 });
 
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/show', {campground});
+    res.render('campgrounds/show', { campground });
 });
 
 app.put('/campgrounds/:id', async (req, res) => {
-    const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.delete('/campgrounds/:id', async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 });
