75ab9d20a4ae510b7db33aff6470111673666cdb
Samuel Perez <sperez207@gmail.com>
2023-07-27 18:03:36 -0400

    Created a public directory and served it in app Now scripts can be imported from it so some things were moved there to clean up the project a bit.

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ const reviews = require('./routes/reviews');
 
 const ExpressError = require('./utils/ExpressError');
 
-// MODELS
-const Campground = require('./models/campground');
-const Review = require('./models/review');
-
 // MONGOOSE
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
@@ -30,6 +26,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // ROUTES
 app.get('/', (req, res) => {
