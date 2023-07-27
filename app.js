cc2ec6529f4386e3b758e4631a6e338c1310c82c
Samuel Perez <sperez207@gmail.com>
2023-07-27 18:13:41 -0400

    Response now includes a cookie for a session

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
-
 const ExpressError = require('./utils/ExpressError');
+const session = require('express-session');
 
 // MONGOOSE
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
@@ -24,9 +24,22 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret: 'temporary',
+    resave: false,
+    saveUninitialized: true,
+    // store: TODO change to actual production store eventually, currently default memory store
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + (1000 * 60 * 60 * 24 * 7),
+        maxAge: (1000 * 60 * 60 * 24 * 7)
+    }
+};
+app.use(session(sessionConfig));
 
 // ROUTES
 app.get('/', (req, res) => {
