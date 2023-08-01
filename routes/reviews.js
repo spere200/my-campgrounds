06b3c9076d357bc39db4b36b7efacc6eed23eaa1
Samuel Perez <sperez207@gmail.com>
2023-08-01 16:58:48 -0400

    Added a user controller.

--- routes/reviews.js
+++ routes/reviews.js
@@ -8,6 +8,8 @@ const reviews = require('../controllers/reviews');
 // REVIEW ROUTES
 router.post('/', isLoggedIn, validateReview, catchAsync(reviews.create));
 router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.delete));
-router.get('*', reviews.returnTo); // only here to have a safe returnTo in some edge cases
+
+// since there is now show page for reviews, this is here as a catch all for returnTo
+router.get('*', reviews.returnTo);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,58 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
 const { storeReturnTo } = require('../middleware');
+const users = require('../controllers/users');
 
-router.get('/register', (req, res, next) => {
-    res.render('users/register');
-});
-
-router.post('/register', catchAsync(async (req, res, next) => {
-    try {
-        const { email, username, password } = req.body.user;
-        const user = new User({ email, username });
-        const registeredUser = await User.register(user, password);
-
-        req.login(registeredUser, (err) => {
-            if (err) {
-                return next(err)
-            }
-            else {
-                req.flash('success', 'You are now logged in.');
-                res.redirect('/campgrounds');
-            }
-        });
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('/register');
-    }
-}));
-
-router.get('/login', (req, res, next) => {
-    res.render('users/login');
-});
+router.get('/register', users.renderRegisterForm);
+router.post('/register', catchAsync(users.create));
+router.get('/login', users.renderLoginForm);
 
 // passport.authenticate wipes anything stored on the session for security reasons, so storeReturnTo
 // takes whatever is stored in the session and transfers it to the response locals (res.local), so
 // that the url the user was on can be stored before passport.authenticate wipes it.
-router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res, next) => {
-    req.flash('success', 'Successfully logged in.');
-    const redirectUrl = res.locals.returnTo || '/campgrounds';
-    res.redirect(redirectUrl);
-});
+router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 
-router.get('/logout', (req, res, next) => {
-    req.logout((err) => {
-        if (err) {
-            next(err)
-        }
-        else {
-            req.flash('success', 'Logged out succesfully.');
-            res.redirect('/campgrounds');
-        }
-    });
-});
+router.get('/logout', users.logout);
 
 module.exports = router;
