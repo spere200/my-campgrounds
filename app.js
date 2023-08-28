4bd11711f5e7065c94fa0fdcb36959e2b415c5ff
Samuel Perez <sperez207@gmail.com>
2023-08-28 01:17:12 -0400

    Changed the style of the image upload button Imported "bs-custom-file-input" as a script in boilerplate.ejs and initialized it in validateForms.js

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(flash());
 // already called in the session/flash section
 app.use(passport.initialize());
 app.use(passport.session()); 
-passport.use(new LocalStrategy(User.authenticate()))
+passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
