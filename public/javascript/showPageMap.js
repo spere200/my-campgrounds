b8eee6cd3de6f94e821e47ea7aaaab5575eb5504
Samuel Perez <sperez207@gmail.com>
2023-09-06 03:01:50 -0400

    Finished styling maps on the index and show pages

--- public/javascript/showPageMap.js
+++ public/javascript/showPageMap.js
@@ -1,11 +1,13 @@
 mapboxgl.accessToken = mapToken;
 
 var map = new mapboxgl.Map({
-    container: 'map',
+    container: 'show-map',
     style: 'mapbox://styles/mapbox/light-v11',
     center: campground.geometry.coordinates,
     zoom: 10
 });
+
+map.addControl(new mapboxgl.NavigationControl());
 
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
